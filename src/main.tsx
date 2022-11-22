b3a8824fc27434c522a9a2fbceb63f0c5684974c
shine-lzx <1597395958@qq.com>
2022-11-22 10:49:28 +0800

    add sentry

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,14 @@ import 'antd/dist/antd.less'
 import './index.css'
 import 'virtual:svg-icons-register'
 import 'moment/locale/zh-cn'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
+Sentry.init({
+  dsn: 'https://71141835d595464ab5bf1f1b5b99a5b5@o4504200460632064.ingest.sentry.io/4504200471379968',
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ConfigProvider locale={zhCN}>
--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import styles from './index.module.scss'
+import { Button } from 'antd'
 
 export default () => {
   return (
@@ -6,6 +7,7 @@ export default () => {
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt voluptate neque repellendus laudantium iusto,
       quasi distinctio, dicta accusantium pariatur assumenda ratione optio quisquam ut eos. Ducimus eos laboriosam
       sapiente veritatis.
+      <Button onClick={() => {}}>add</Button>
     </div>
   )
 }
